89bee78dd42212ad69ab89e97891e3e1be03cc10
Orlando Ernesto Abreu Cruz <orlando.cruz@cmas-systems.com>
2025-02-27 12:10:34 +0000

    [ i ] modify adding link to queue commit

--- usuario/src/main/java/com/coface/usuario/UsuarioApp.java
+++ usuario/src/main/java/com/coface/usuario/UsuarioApp.java
@@ -3,7 +3,7 @@ package com.coface.usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.coface.usuario")
+@SpringBootApplication
 public class UsuarioApp {
     public static void main( String[] args ) {
         SpringApplication.run(UsuarioApp.class, args);
